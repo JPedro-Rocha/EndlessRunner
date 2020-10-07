eb4c1c60e589db3faf6a057bdedb1e2d94c771fb
JPedro-Rocha <jp.oliveirarocha@hotmail.com>
2020-10-07 18:32:32 -0300

    scenes

---
+++ EndlessRunner-Unity/Assets/Scripts/movimentoPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentoPlayer : MonoBehaviour
+{
+    private CharacterController controle;
+    public float velocidade;
+    public float alturaPulo;
+    private float velocidadePulo;
+    public float gravidade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controle = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.forward * velocidade;
+        if(controle.isGrounded){
+
+            if(Input.GetKeyDown(KeyCode.Space)){
+                velocidadePulo = alturaPulo;
+            }
+        }
+
+        else{
+            velocidadePulo -= gravidade;
+        }
+
+        direction.y = velocidadePulo;
+        controle.Move(direction * Time.deltaTime);
+    } 
+}

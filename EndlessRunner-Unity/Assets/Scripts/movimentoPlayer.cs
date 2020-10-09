cc728552045d69b8a86576e9cd9a2f9ce22e303e
Moises1605 <56726033+Moises1605@users.noreply.github.com>
2020-10-09 16:37:01 -0300

    movimento para frente e para os lados e pulo sem animação

--- EndlessRunner-Unity/Assets/Scripts/movimentoPlayer.cs
+++ EndlessRunner-Unity/Assets/Scripts/movimentoPlayer.cs
@@ -9,6 +9,7 @@ public class movimentoPlayer : MonoBehaviour
     public float alturaPulo;
     private float velocidadePulo;
     public float gravidade;
+    public float velocidadeHorizontal;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,16 @@ public class movimentoPlayer : MonoBehaviour
 
             if(Input.GetKeyDown(KeyCode.Space)){
                 velocidadePulo = alturaPulo;
+            } 
+
+            if(Input.GetKeyDown(KeyCode.RightArrow)){
+                StartCoroutine(movementoDireto());
+                //direita();
+            }
+
+            if(Input.GetKeyDown(KeyCode.LeftArrow)){
+                 StartCoroutine(movimentoEsquerdo());
+                 //esquerda();
             }
         }
 
@@ -34,4 +45,31 @@ public class movimentoPlayer : MonoBehaviour
         direction.y = velocidadePulo;
         controle.Move(direction * Time.deltaTime);
     } 
+
+    IEnumerator movimentoEsquerdo()
+    {
+        for(float i = 0; i < 2; i += 0.2f)
+        {
+            controle.Move(Vector3.left * Time.deltaTime * velocidadeHorizontal);
+            yield return null;
+        } 
+
+    }
+
+    IEnumerator movementoDireto()
+    {
+          for(float i = 0; i < 2; i += 0.2f)
+        {
+            controle.Move(Vector3.right * Time.deltaTime * velocidadeHorizontal);
+            yield return null;
+        }
+    } 
+
+    void esquerda(){
+        controle.Move(Vector3.left * Time.deltaTime * velocidadeHorizontal);
+    }
+
+    void direita(){
+         controle.Move(Vector3.right * Time.deltaTime * velocidadeHorizontal);
+    }
 }

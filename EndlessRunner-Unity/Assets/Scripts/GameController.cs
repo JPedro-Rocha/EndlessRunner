82ac444be69062718e9ecfa4fe64b91304f2d6b8
Moises1605 <56726033+Moises1605@users.noreply.github.com>
2020-10-12 15:48:33 -0300

    recorde e velocidade constante

---
+++ EndlessRunner-Unity/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    private float recorde;
+    public movimentoPlayer jogador;
+    public Text recordeTela;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jogador = GameObject.FindGameObjectWithTag("Player").GetComponent<movimentoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        recorde += Time.deltaTime * 5f;
+        recordeTela.text = Mathf.Round(recorde).ToString();
+        jogador.velocidade = recorde/10;
+    }
+}

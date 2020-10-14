82631ff78c1d4e4a53d9fe8e82b2a6b697d9d669
JPedro-Rocha <jp.oliveirarocha@hotmail.com>
2020-10-13 21:32:51 -0300

    obstáculos randomizados

--- EndlessRunner-Unity/Assets/Scripts/spawnObstacles.cs
+++ EndlessRunner-Unity/Assets/Scripts/spawnObstacles.cs
@@ -33,8 +33,45 @@ public class spawnObstacles : MonoBehaviour
         {
             float posZMin = -40.42f;//(66.84f / novoObstaculo.Count) + (66.84f / novoObstaculo.Count) * i;
             float posZMax = 20.42f;//(66.84f / novoObstaculo.Count) + (66.84f / novoObstaculo.Count) * i + 1;
-            novoObstaculo[i].transform.localPosition = new Vector3(0, 0.35f, Random.Range(posZMin, posZMax));
-            novoObstaculo[i].SetActive(true);
+
+            //novoObstaculo[i].transform.localPosition = new Vector3(0, 0.35f, Random.Range(posZMin, posZMax));
+            //novoObstaculo[i].SetActive(true);
+
+            float[] posXBarril = { 0.67f, 2.79f, 4.9f };
+
+            float[] posXConsole = { 0.673f, 2.778f, 4.874f };
+
+            float[] posYBarras = { 0.18f, -2.31f };
+
+            float[] posXProjetor = { 1.9f, 4.67f };
+
+            if (novoObstaculo[i].tag == "Barril")
+            {
+                novoObstaculo[i].transform.localPosition = new Vector3(posXBarril[Random.Range(0, posXBarril.Length)],
+                                                                         -0.18f, Random.Range(posZMin, posZMax));
+                novoObstaculo[i].SetActive(true);
+            }
+
+            else if (novoObstaculo[i].tag == "Console")
+            {
+                novoObstaculo[i].transform.localPosition = new Vector3(posXConsole[Random.Range(0, posXConsole.Length)],
+                                                                         0.477f, Random.Range(posZMin, posZMax));
+                novoObstaculo[i].SetActive(true);
+            }
+
+            else if (novoObstaculo[i].tag == "Barras")
+            {
+                novoObstaculo[i].transform.localPosition = new Vector3(4.35f,
+                                         posYBarras[Random.Range(0, posYBarras.Length)], Random.Range(posZMin, posZMax));
+                novoObstaculo[i].SetActive(true);
+            }
+
+            else if (novoObstaculo[i].tag == "Projetor")
+            {
+                novoObstaculo[i].transform.localPosition = new Vector3(posXProjetor[Random.Range(0, posXProjetor.Length)],
+                                                                         -0.22f, Random.Range(posZMin, posZMax));
+                novoObstaculo[i].SetActive(true);
+            }
         }
     }
 }
